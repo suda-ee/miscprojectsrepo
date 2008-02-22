ca0694af1a53ad31e52cd14de642abcec4cb5db5
cai <cai@localhost>
2008-02-22 02:59:39 +0000

    psi????t<0??0

--- ptdemfie/psi_func.F90
+++ ptdemfie/psi_func.F90
@@ -14,12 +14,16 @@ real t, psi_func
     integer j
     real laguerre_t(0:i_rank)
     ! Excutives
-    laguerre_t(0)=exp(-t/2._DKIND)
-    if (i_rank>=1) then
-        laguerre_t(1)=(1._DKIND-t)*laguerre_t(0)
+    if (t<0.) then
+        laguerre_t(i_rank) = 0.
+    else
+        laguerre_t(0)=exp(-t/2._DKIND)
+        if (i_rank>=1) then
+            laguerre_t(1)=(1._DKIND-t)*laguerre_t(0)
+        end if
+        do j=2, i_rank
+            laguerre_t(j)=((2*j-1-t)*laguerre_t(j-1)-(j-1)*laguerre_t(j-2))/j
+        end do
     end if
-    do j=2, i_rank
-        laguerre_t(j)=((2*j-1-t)*laguerre_t(j-1)-(j-1)*laguerre_t(j-2))/j
-    end do
     psi_func = laguerre_t(i_rank)
 end function psi_func
