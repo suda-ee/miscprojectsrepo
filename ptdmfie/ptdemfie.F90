17a4789aa791d6eef9a00ef8c4e19955af743b68
cai <cai@localhost>
2008-03-26 02:31:22 +0000

    ?????? feko ??

--- ptdmfie/ptdemfie.F90
+++ ptdmfie/ptdemfie.F90
@@ -96,7 +96,7 @@ character*64 filebasename
     open(unit=1503+iam,file=trim(filebasename)//'.part',status='old', action='read')
     read(1503+iam,*) num_edges
     allocate(edge(num_edges))
-    read(1503+iam,*) (edge(time)%tri(1), edge(time)%tri(2), edge(time)%poi(1), &
+    read(1503+iam,*) (edge(time)%tri(2), edge(time)%tri(1), edge(time)%poi(1), &
         edge(time)%poi(3), edge(time)%res_angle, time=1, num_edges)
     close(1503+iam)
     call tran(point, triangle, edge)
